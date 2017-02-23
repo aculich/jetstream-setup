52dc52e8ccf987b3609cec6b6425b8170d448a86
Aaron Culich <aculich@gmail.com>
2017-02-23 14:36:38 -0800

    install docker for Ubuntu 14.04
    https://docs.docker.com/engine/installation/linux/ubuntu/

---
+++ jetstream-setup.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+## Run this script as root (with sudo)
+
+## From official Docker recommendations for installing on Ubuntu 14.04 (trusty):
+##    https://docs.docker.com/engine/installation/linux/ubuntu/
+
+apt-get update
+
+## Recommended extra packages for Trusty 14.04¶
+apt-get install -y --no-install-recommends \
+    linux-image-extra-$(uname -r) \
+    linux-image-extra-virtual
+
+## Set up the repository
+apt-get install -y --no-install-recommends \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    software-properties-common
+
+curl -fsSL https://apt.dockerproject.org/gpg | sudo apt-key add -
+apt-key fingerprint 58118E89F3A912897C070ADBF76221572C52609D
+sudo add-apt-repository \
+       "deb https://apt.dockerproject.org/repo/ \
+       ubuntu-$(lsb_release -cs) \
+       main"
+
+sudo apt-get update
+sudo apt-get -y install docker-engine
